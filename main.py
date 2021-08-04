a937ab9df5fee699eac8ce1cfdf6776fe68acd24
psztefko <p.sztefko@gmail.com>
2021-08-04 14:54:54 +0200

    gets current track info

--- main.py
+++ main.py
@@ -1,3 +1,50 @@
+import time
+from pprint import pprint
+from env import SPOTIFY_ACCESS_TOKEN, SPOTIFY_GET_CURRENT_TRACK_URL
+import requests
+
+
+def get_current_track(access_token):
+    response = requests.get(
+        SPOTIFY_GET_CURRENT_TRACK_URL,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+    json_resp = response.json()
+
+    track_id = json_resp['item']['id']
+    track_name = json_resp['item']['name']
+    artists = [artist for artist in json_resp['item']['artists']]
+
+    link = json_resp['item']['external_urls']['spotify']
+
+    artist_names = ', '.join([artist['name'] for artist in artists])
+
+    current_track_info = {
+        "id": track_id,
+        "track_name": track_name,
+        "artists": artist_names,
+        "link": link
+    }
+
+    return current_track_info
+
+
+def main():
+    current_track_id = None
+    while True:
+        current_track_info = get_current_track(SPOTIFY_ACCESS_TOKEN)
+
+        if current_track_info['id'] != current_track_id:
+            pprint(
+                current_track_info,
+                indent=4,
+            )
+            current_track_id = current_track_info['id']
+
+        time.sleep(2)
+
 
 if __name__ == '__main__':
-    pass
+    main()
