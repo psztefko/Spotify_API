aad0665fdfc853f503c29599e757af32fcb0c41d
psztefko <p.sztefko@gmail.com>
2021-08-06 14:37:31 +0200

    added requirements.txt and changed file structure

---
+++ env.py
@@ -0,0 +1,5 @@
+
+
+SPOTIFY_USER_ID = "piotrex2308?si=1adca27567dd40a6"
+SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
+SPOTIFY_ACCESS_TOKEN = "BQAwQeOrtFe_mtpcaR6hMsIamd5oqp0I5DLiuYKElsXCCneUb_eLVJk1VRx1wkFUrqcX1LnORUimjzBjia3NjfY1y7z1mVw5KHvMGqqnZNm84q4gl"
