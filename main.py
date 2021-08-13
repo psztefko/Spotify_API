eb6deaacb196cbb367745852ff0ca78f2a99e345
Piotrek <p.sztefko@gmail.com>
2021-08-13 15:04:01 +0200

    fresh start

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-from env import CLIENT_ID, CLIENT_SECRET
-from src.Spotify import CreatePlaylist
-import time
-from src.Authorization import Authorization
+import argparse
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+from env import SCOPE
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
 
 def main():
 
-    spotify = CreatePlaylist()
+    hello_world()
+    # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
+    #
+    # results = sp.current_user_saved_tracks()
+    # for idx, item in enumerate(results['items']):
+    #     track = item['track']
+    #     print(idx, track['artists'][0]['name'], " – ", track['name'])
 
-    current_track_id = None
-    while True:
-        current_track_info = spotify.get_current_track()
 
-        if current_track_info['id'] != current_track_id:
-            print(current_track_info)
-            spotify.add_song_to_playlist(current_track_info['uri'], spotify.get_playlist_id())
-            current_track_id = current_track_info['id']
 
-        time.sleep(2)
+
+
+    # spotify = CreatePlaylist()
+    #
+    # current_track_id = None
+    # while True:
+    #     current_track_info = spotify.get_current_track()
+    #
+    #     if current_track_info['id'] != current_track_id:
+    #         print(current_track_info)
+    #         spotify.add_song_to_playlist(current_track_info['uri'], spotify.get_playlist_id())
+    #         current_track_id = current_track_info['id']
+    #
+    #     time.sleep(2)
+
 
 
 if __name__ == '__main__':
