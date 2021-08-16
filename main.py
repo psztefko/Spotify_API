428a26802fca6011afa65bac2279652ce27b60b2
Piotrek <p.sztefko@gmail.com>
2021-08-16 11:57:57 +0200

    fully functional version

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-import argparse
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-from env import SCOPE
-from flask import Flask
+import time
+import spotipy as spotipy
+from spotipy import SpotifyOAuth
+import logging
 
-app = Flask(__name__)
+from env import USER_ID
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+SCOPE="playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-read-currently-playing"
 
+logger = logging.getLogger('app')
+logging.basicConfig(level='DEBUG')
 
-def main():
-
-    hello_world()
-    # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
-    #
-    # results = sp.current_user_saved_tracks()
-    # for idx, item in enumerate(results['items']):
-    #     track = item['track']
-    #     print(idx, track['artists'][0]['name'], " – ", track['name'])
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
 
 
+def get_playlist_id():
 
+    # gets list of user playlists
+    playlists = sp.current_user_playlists()['items']
 
+    # iterate though playlists to check if 'agent of chaos' exists
+    for playlist in range(len(playlists)):
+        if playlists[playlist]['name'] == 'agent of chaos':
+            logger.info('Playlist already exists')
+            return playlists[playlist]['id']
 
-    # spotify = CreatePlaylist()
-    #
-    # current_track_id = None
-    # while True:
-    #     current_track_info = spotify.get_current_track()
-    #
-    #     if current_track_info['id'] != current_track_id:
-    #         print(current_track_info)
-    #         spotify.add_song_to_playlist(current_track_info['uri'], spotify.get_playlist_id())
-    #         current_track_id = current_track_info['id']
-    #
-    #     time.sleep(2)
-
+    # creates playlist if one doesn't exists
+    return sp.user_playlist_create(USER_ID, "agent of chaos", True, False, "under construction")['id']
 
 
 if __name__ == '__main__':
-    main()
+
+    playlist_id = get_playlist_id()
+
+
+
+    current_track_id = None
+    while True:
+        current_track_info = sp.currently_playing()
+        if current_track_info['item']['id'] != current_track_id:
+            sp.playlist_add_items(playlist_id, [current_track_info['item']['uri']])
+            current_track_id = current_track_info['item']['id']
+
+        time.sleep(50)
